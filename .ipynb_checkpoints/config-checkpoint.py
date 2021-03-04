6cf2021707177bcebb2ee2bc81dff85452c2ab6b
Nicholas Olumese <onyxcollc@gmail.com>
2021-03-04 14:53:48 -0700

    Branch Update

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,4 @@
+DB _HOST = 'localhost'
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'etfdb'
---
+++ config.py
@@ -0,0 +1,10 @@
+API_URL = 'https://paper-api.alpaca.markets'
+API_KEY = "PKG3Y0LH37O5KB92QOX5"
+API_SECRET = "wHeHAo4mpe5kGjk33fwnCXmbio6NJ8ZCtKYmoD0S"
+
+
+
+DB _HOST = 'localhost'
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'etfdb'
